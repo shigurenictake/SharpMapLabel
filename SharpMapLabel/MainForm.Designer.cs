c53dafa190db35cd231bbd64ec61f71a9ff2cee7
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-07-09 01:10:13 +0900

    不要オブジェクト削除

--- SharpMapLabel/MainForm.Designer.cs
+++ SharpMapLabel/MainForm.Designer.cs
@@ -37,15 +37,9 @@ namespace SharpMapLabel
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
             this.mb = new SharpMap.Forms.MapBox();
             this.mapZoomToolStrip1 = new SharpMap.Forms.ToolBar.MapZoomToolStrip(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.txtImgGeneration = new System.Windows.Forms.TextBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStripContainer1
@@ -55,12 +49,12 @@ namespace SharpMapLabel
             // 
             this.toolStripContainer1.ContentPanel.Controls.Add(this.mb);
             this.toolStripContainer1.ContentPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(676, 533);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(876, 533);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.toolStripContainer1.Location = new System.Drawing.Point(200, 0);
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
             this.toolStripContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.toolStripContainer1.Name = "toolStripContainer1";
-            this.toolStripContainer1.Size = new System.Drawing.Size(676, 558);
+            this.toolStripContainer1.Size = new System.Drawing.Size(876, 558);
             this.toolStripContainer1.TabIndex = 7;
             this.toolStripContainer1.Text = "toolStripContainer1";
             // 
@@ -85,7 +79,7 @@ namespace SharpMapLabel
             this.mb.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mb.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mb.ShowProgressUpdate = false;
-            this.mb.Size = new System.Drawing.Size(676, 533);
+            this.mb.Size = new System.Drawing.Size(876, 533);
             this.mb.TabIndex = 6;
             this.mb.Text = "mb";
             this.mb.WheelZoomMagnitude = -2D;
@@ -101,64 +95,12 @@ namespace SharpMapLabel
             this.mapZoomToolStrip1.TabIndex = 0;
             this.mapZoomToolStrip1.Text = "MapZoomToolStrip";
             // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textBox1.Location = new System.Drawing.Point(0, 513);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 45);
-            this.textBox1.TabIndex = 8;
-            this.textBox1.Text = "NB: Tree view has layer-sensitive context menu to interact with Variable Layers a" +
-    "nd Map Point Layers";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.txtImgGeneration);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 558);
-            this.panel1.TabIndex = 9;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 471);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 42);
-            this.panel2.TabIndex = 10;
-            // 
-            // txtImgGeneration
-            // 
-            this.txtImgGeneration.Dock = System.Windows.Forms.DockStyle.Top;
-            this.txtImgGeneration.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtImgGeneration.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.txtImgGeneration.Location = new System.Drawing.Point(0, 0);
-            this.txtImgGeneration.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtImgGeneration.Multiline = true;
-            this.txtImgGeneration.Name = "txtImgGeneration";
-            this.txtImgGeneration.Size = new System.Drawing.Size(200, 35);
-            this.txtImgGeneration.TabIndex = 9;
-            this.txtImgGeneration.Text = "Image generation mode:";
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(876, 558);
             this.Controls.Add(this.toolStripContainer1);
-            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -170,8 +112,6 @@ namespace SharpMapLabel
             this.toolStripContainer1.TopToolStripPanel.PerformLayout();
             this.toolStripContainer1.ResumeLayout(false);
             this.toolStripContainer1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -179,11 +119,6 @@ namespace SharpMapLabel
         #endregion
         private SharpMap.Forms.MapBox mb;
         private System.Windows.Forms.ToolStripContainer toolStripContainer1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox txtImgGeneration;
-        private System.Windows.Forms.Panel panel2;
         private SharpMap.Forms.ToolBar.MapZoomToolStrip mapZoomToolStrip1;
     }
 }
