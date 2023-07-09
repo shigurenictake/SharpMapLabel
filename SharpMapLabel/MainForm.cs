4e19af106975fff78299314992dd289542781663
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-07-09 19:35:09 +0900

    微修正

--- SharpMapLabel/MainForm.cs
+++ SharpMapLabel/MainForm.cs
@@ -5,6 +5,7 @@ using SharpMap.Data.Providers;
 using SharpMap.Layers;
 using SharpMap.Rendering.Decoration;
 using System.Drawing;
+using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace SharpMapLabel
@@ -38,16 +39,32 @@ namespace SharpMapLabel
             return res;
         }
 
+        //初期化 背景レイヤ
+        private void InitBackground(Map map)
+        {
+            //レイヤーの作成
+            VectorLayer baseLayer = new VectorLayer("baseLayer");
+            baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\polbnda_jpn\polbnda_jpn.shp");
+
+            baseLayer.Style.Fill = Brushes.LimeGreen;
+            baseLayer.Style.Outline = Pens.Black;
+            baseLayer.Style.EnableOutline = true;
+
+            map.BackgroundLayer.Add(baseLayer);
+        }
+
         //★初期化 バリアブルレイヤ
         private void InitVariableLayers(Map map)
         {
             LayerGroup lyrGrp = null;
             VectorLayer lyr = null;
+            LabelLayer llyr = null;
 
             // group layer with single target + labels
-            lyrGrp = new LayerGroup("Fast Boats Group");
+            lyrGrp = new LayerGroup("layGrp");
+
             //ターゲットレイヤの生成 {カラム1,カラム2,…}
-            string name = "Fast Boats";
+            string name = "layTarget";
             System.Data.DataColumn[] columns = new[] { new System.Data.DataColumn("Name", typeof(string)) };
             {
                 var fdt = new FeatureDataTable();
@@ -55,10 +72,9 @@ namespace SharpMapLabel
                 fdt.Columns.AddRange(columns);
                 lyr = new VectorLayer(name, new GeometryFeatureProvider(fdt));
             }
-
             lyr.Style.PointColor = new SolidBrush(Color.Yellow);
+
             //ラベルレイヤ生成、ターゲットレイヤに紐づけ
-            LabelLayer llyr;
             string column = "Name";
             bool enabled = true;
             {
@@ -72,7 +88,7 @@ namespace SharpMapLabel
             }
 
             //文字列をポイント(ジオメトリ)に付与
-            name = "Fast 1あああ";
+            string labelText = "Fast 1あああ";
             //ポイント(ジオメトリ)生成
             NetTopologySuite.Geometries.Point startAt = new NetTopologySuite.Geometries.Point(new Coordinate(135.3, 36));
             //オブジェクト生成
@@ -80,7 +96,7 @@ namespace SharpMapLabel
                 var fp = (GeometryFeatureProvider)lyr.DataSource;
                 var fdr = fp.Features.NewRow();
                 //カラムと同順に情報を設定
-                fdr[0] = name; //★Name ラベル
+                fdr[0] = labelText; //★ ラベル
                 fdr.Geometry = startAt; //ジオメトリを設定
                 fp.Features.AddRow(fdr);
             }
@@ -89,20 +105,6 @@ namespace SharpMapLabel
             lyrGrp.Layers.Add(lyr);
             lyrGrp.Layers.Add(llyr);
             map.VariableLayers.Add(lyrGrp);
-        }
-
-        //初期化 背景レイヤ
-        private void InitBackground(Map map)
-        {
-            //レイヤーの作成
-            VectorLayer baseLayer = new VectorLayer("baseLayer");
-            baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\polbnda_jpn\polbnda_jpn.shp");
-
-            baseLayer.Style.Fill = Brushes.LimeGreen;
-            baseLayer.Style.Outline = Pens.Black;
-            baseLayer.Style.EnableOutline = true;
-
-            map.BackgroundLayer.Add(baseLayer);
         }
     }
 }
